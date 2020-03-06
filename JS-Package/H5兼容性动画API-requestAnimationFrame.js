0896b183593834724e8ef58dda918ff3b27de3be
Brannua <374453156@qq.com>
2020-03-06 14:12:46 +0800

    ok

--- JS-Package/H5兼容性动画API-requestAnimationFrame.js
+++ JS-Package/H5兼容性动画API-requestAnimationFrame.js
@@ -1,16 +1,16 @@
 window.requestAnimationFrame = (function () {
-    return window.requestAnimationFrame ||
-        window.webkitRequestAnimationFrame ||
-        window.mozRequestAnimationFrame ||
-        function (callback) {
-            window.setTimeout(callback, 1000 / 60);
-        }
+  return window.requestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    function (callback) {
+      window.setTimeout(callback, 1000 / 60); // 页面每秒刷新60次
+    }
 }());
 window.cancelAnimationFrame = (function () {
-    return window.cancelAnimationFrame ||
-        window.webkitcancelAnimationFrame ||
-        window.mozcancelAnimationFrame ||
-        function (id) {
-            window.clearTimeout(id);
-        }
+  return window.cancelAnimationFrame ||
+    window.webkitcancelAnimationFrame ||
+    window.mozcancelAnimationFrame ||
+    function (id) {
+      window.clearTimeout(id);
+    }
 }());
