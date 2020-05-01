d3069e885fa23d0a9ec15b1ce4bf2bc714da7204
Brannua <374453156@qq.com>
2020-05-01 18:46:19 +0800

    feature: 深拷贝复习

--- JS-Package/clone.js
+++ JS-Package/clone.js
@@ -1,84 +1,31 @@
 /**
- * 自封装clone-API
- * author: lpj
+ * 深拷贝
+ * @param {*} origin 被拷贝的数据
+ * origin == null 相当于 origin === null || origin === undefined
  */
+function deepClone(origin) {
+  // 原始值直接返回
+  if (typeof(origin) !== 'object' || origin == null) {
+    return origin
+  }
 
-var clone = {
-  /**
-   * 浅克隆
-   * @param {Object} origin 被克隆的对象
-   * @param {Object} target 克隆出来的对象
-   */
-  simpleClone(origin, target) {
-    var target = target || {};
-    for (var prop in origin) {
-      if (origin.hasOwnProperty(prop)) {
-        target[prop] = origin[prop];
-      }
+  // 初始化返回值
+  let result
+  if (origin instanceof Array) {
+    result = []
+  }
+  if (origin instanceof Object) {
+    result = {}
+  }
+
+  // 遍历
+  for (let key in origin) {
+    // 排除原型链上的属性
+    if (origin.hasOwnProperty(key)) {
+      // 递归
+      result[key] = deepClone(origin[key])
     }
-    return target;
-  },
+  }
 
-  /**
-   * 深克隆( 引用值只考虑 {} 和 [] )
-   * 原始值 : number, string, boolean, undefined, null
-   * @param {Object} origin 被克隆的对象
-   * @param {Object} target 克隆出来的对象
-   */
-  deepClone_1(origin, target) {
-    // 参数兼容处理
-    var target = target || {};
-    // 遍历属性名
-    for (var prop in origin) {
-      // 不拷贝原型上的属性
-      if (origin.hasOwnProperty(prop)) {
-        // 获取属性值
-        var temp = origin[prop];
-        // 处理引用值
-        if (typeof (temp) === "object" && temp !== null) {
-          if (temp instanceof Array) {
-            target[prop] = deepClone(temp, []);
-          } else {
-            target[prop] = deepClone(temp, {});
-          }
-        } else {
-          // 处理原始值
-          target[prop] = temp;
-        }
-      }
-    }
-    return target;
-  },
-
-
-  /**
-  * 深克隆( 引用值只考虑 {} 和 [] )
-  * 原始值 : number, string, boolean, undefined, null
-  * @param {Object} origin 被克隆的对象
-  * @param {Object} target 克隆出来的对象
-  */
-  deepClone_2(origin, target) {
-    // 参数兼容处理
-    var target = target || {},
-      _toStr = Object.prototype.toString;
-    // 遍历属性名
-    for (var prop in origin) {
-      // 不拷贝原型上的属性
-      if (origin.hasOwnProperty(prop)) {
-        // 获取属性值
-        var temp = origin[prop];
-        // 处理引用值
-        if (typeof (temp) === "object" && temp !== null) {
-          target[prop] = _toStr.call(temp) === "[object Array]" ? [] : {};
-          target[prop] = deepClone_2(temp, target[prop]);
-        } else {
-          // 处理原始值
-          target[prop] = temp;
-        }
-      }
-    }
-    return target;
-  },
-};
-
-module.exports = clone;
+  return result
+}
